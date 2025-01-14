90fa29efa26aa8405603e02a900831cd0146bff3
Devy Salomon <devysalomon@gmail.com>
2025-01-14 16:21:32 -0500

    se actualizo el codemap por si es necesario cambiar los valores

--- src/tcp/data.service.ts
+++ src/tcp/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { WebsocketGateway } from 'src/websocket/websocket.gateway';
+import { codeMap } from './code-map';
 
 @Injectable()
 export class DataService {
@@ -7,30 +8,7 @@ export class DataService {
 
   constructor(private websocketGateway: WebsocketGateway) {}
 
-  private codeMap = {
-    '0108': 'DEPTH',
-    '0110': 'HOLE_DEPTH',
-    '0112': 'BLOCK_HEIGHT',
-    '0113': 'ROP',
-    '0114': 'HOOKLOAD',
-    '0145': 'SLIPS',
-    '0144': 'ON_BOTTOM',
-    '0120': 'RPM',
-    '0130': 'FLOW',
-    '0121': 'SPP',
-    '0123': 'SPM1',
-    '0124': 'SPM2',
-    '0116': 'WOB',
-    '0118': 'TORQ',
-    '0713': 'INC',
-    '0714': 'AZM',
-    '0715': 'AZMC',
-    '0722': 'GTOT',
-    '0723': 'BTOT',
-    '0724': 'DIP',
-    '0717': 'TF',
-    '0823': 'GAM',
-  };
+  private codeMap = codeMap;
 
   processData(data: string) {
     const result = {};
